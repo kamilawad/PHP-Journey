d1d0f97f88ac872336e68ac2dafb171f6e6b6936
kamilawad1 <kamilawad15@gmail.com>
2024-03-12 04:01:13 +0200

    solved decimal to binary problem

--- decimalToBinary.php
+++ decimalToBinary.php
@@ -1,5 +1,12 @@
 <?php
 
 function decimalToBinary($decimal) {
-    
+    $binary = '';
+    while ($decimal > 0) {
+        $binary .= $decimal % 2;
+        $decimal = (int)($decimal / 2);
+    }
+    return strrev($binary);
 }
+
+echo decimalToBinary(16);
---
+++ mergeSort.php
@@ -0,0 +1,5 @@
+<?php
+
+function mergeSort($arr) {
+    
+}
